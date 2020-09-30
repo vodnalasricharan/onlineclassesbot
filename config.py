bc93caf780bf8ac68ee2c4cb361667bd13dab1e8
Sricharan <vodnalasricharan@gmail.com>
2020-09-30 12:15:56 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+class Config(object):
+	BOT_TOKEN = os.environ.get('BOT_TOKEN','1314925010:AAFLf0g8QfGtw3-OCEVAYnv5p8rwFCvZznk')
+	USERNAME = os.environ.get('USERNAME','charansrikar99@gmail.com')
+	PASSWORD = os.environ.get('PASSWORD','6303994496')
