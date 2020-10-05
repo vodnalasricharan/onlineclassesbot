26c66a7dec3de3c8e91688fc4cd59d38a21c746a
Sricharan <vodnalasricharan@gmail.com>
2020-10-05 18:00:16 +0530

    #4

--- config.py
+++ config.py
@@ -3,7 +3,9 @@ class Config(object):
 	BOT_TOKEN = os.environ.get('BOT_TOKEN')
 	USERNAME = os.environ.get('USERNAME')
 	PASSWORD = os.environ.get('PASSWORD')
+	#comment above 3 lines
 	#If above lines wont work then use below and pass credentials
+	#uncomment below 3 lines
 	# BOT_TOKEN = os.environ.get('BOT_TOKEN','TELEGRAM_BOT_API_TOKEN')
 	# USERNAME = os.environ.get('USERNAME','MAIL_ID')
 	# PASSWORD = os.environ.get('PASSWORD','MAIL_PASSWORD')
